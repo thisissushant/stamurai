cc692062e3db0febe53952b31b862e0c5154432b
Sushant Singh <sushantbibhu@gmail.com>
2024-04-11 03:48:35 +0530

    Update dependencies, add DaisyUI, and modify Home page
    * Upgrade Next.js, React, and React DOM to version 18
    * Add TypeScript typings for Node.js version 20
    * Install DaisyUI and its dependencies
    * Configure ESLint and TypeScript
    * Update Home page with new content and layout
    * Update package-lock.json with new dependencies and versions

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import React from "react";
+
+export const Navbar: React.FC = () => {
+  return (
+    <div className="navbar bg-purple-700 text-white">
+      <div className="flex-1">
+        <a className="font-semibold text-xl">Weather Forecast</a>
+      </div>
+      <div className="flex-none">
+        <button className="btn btn-square btn-ghost">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block w-5 h-5 stroke-current"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+            ></path>
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+};
